2524a8c52716fa87a9c3d7ac5fde273c6f8ae3c1
Maria Fernanda Morales <112129254+cyberialyr777@users.noreply.github.com>
2025-08-17 14:15:21 -0600

    library module createddd

--- library_app/models/library_model.py
+++ library_app/models/library_model.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 from odoo import models, fields, api
 
@@ -6,13 +7,14 @@ class LibraryModel(models.Model):
     _description = 'Library Model'
 
     name = fields.Char(string='Name', default="Test Request")
-    response = fields.Text(string='API Response')
+    response = fields.Text(string='API Response', readonly=True)
 
     def action_call_api(self):
+        self.ensure_one()
         try:
             api_url = "https://jsonplaceholder.typicode.com/todos/1"
             response = requests.get(api_url)
-            response.raise_for_status()
+            response.raise_for_status()  # This will raise an HTTPError for bad responses (4xx or 5xx)
             self.response = response.text
         except requests.exceptions.RequestException as e:
             self.response = f"Error: {e}"
